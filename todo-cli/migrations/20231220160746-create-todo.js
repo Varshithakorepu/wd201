f512179ceb464e7fc4baa6efeb7a606044b53763
Varshithakorepu <151618956+Varshithakorepu@users.noreply.github.com>
2023-12-20 21:46:33 +0530

    Added database files

--- todo-cli/migrations/20231220160746-create-todo.js
+++ todo-cli/migrations/20231220160746-create-todo.js
@@ -1,7 +1,8 @@
 'use strict';
+
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up(queryInterface, Sequelize) {
+  async up (queryInterface, Sequelize) {
     await queryInterface.createTable('Todos', {
       id: {
         allowNull: false,
@@ -28,7 +29,8 @@ module.exports = {
       }
     });
   },
-  async down(queryInterface, Sequelize) {
+
+  async down (queryInterface, Sequelize) {
     await queryInterface.dropTable('Todos');
   }
 };
